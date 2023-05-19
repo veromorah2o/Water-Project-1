38d3fa2ef19779d88abb3586cb985a848e63d2ae
Jaclyn-Nguyen <132543083+Jaclyn-Nguyen@users.noreply.github.com>
2023-05-18 20:38:45 -0700

    app.py
    Add (1 Dimension) to curdoc title

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ inputs = widgetbox(text, menu, Mass)
 layout = row(plot, column(text, menu, Mass))
 
 curdoc().add_root(layout)
-curdoc().title = "Concentration Signals"
+curdoc().title = "Concentration Signals (1 Dimension)"
 
 
 # In[ ]:
